9a8d2d42354d27209bfb6b8825607dea21907634
nicola <nicola.picarella6@gmail.com>
2026-05-27 21:35:38 +0200

    feat: better functions

--- src/uom.js
+++ src/uom.js
@@ -5,9 +5,20 @@ export async function getUnitsOfMeasure(db) {
 }
 
 export async function getUnitOfMeasureIdBySymbol(db, symbol) {
-    return await queryFirstAsync(db, "SELECT id FROM UnitOfMeasure WHERE symbol = " + symbol).id || -1
+    return await queryFirstAsync(db, "SELECT id FROM UnitOfMeasure WHERE symbol = " + symbol).id
 }
 
 export async function getUnitOfMeasureSymbolById(db, id) {
     return await queryFirstAsync(db, "SELECT symbol FROM UnitOfMeasure WHERE id = " + id).symbol
+}
+
+export async function getUnitOfMeasureSymbols(db) {
+    const rows = await queryAllAsync(db, "SELECT symbol FROM UnitOfMeasure")
+
+    let symbols = []
+    for (const row of rows) {
+        symbols.push(row.symbol)
+    }
+
+    return symbols
 }
